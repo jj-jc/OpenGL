5e6a40260c46d9d6e66cd60b755fdd4e28189df4
Juan José Jurado Camino <jj.jurado@alumnos.upm.es>
2021-12-22 16:06:19 +0100

    Added Apuntes.docx to write some notes. Cap1: Vertex Buffer. Cap2: Vertex Attibutes

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -25,10 +25,35 @@ int main(void)
     /* This has to be done after make a valid OpenGL rendering context */
     if (glewInit() != GLEW_OK)
         std::cout << "Error with the glewInit" << std::endl;
+    
+    std::cout << glGetString(GL_VERSION) << std::endl;
 
     unsigned int a;
     glGenBuffers(1, &a);
     
+
+    /* Creation of a Vertex Buffer */
+    /* The points to settle down */
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+    /* Creation of the buffer that return a ID */
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    /* Store the buffer in the GPU memory */
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    /* Start with the atributes of the verteces */
+    /* index, n.componenets, type of components, zise to the next vertex (no the attribute), offset of the first generic vertex attribute*/
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+    /* Enable the vertex attribute 0 */
+    glEnableVertexAttribArray(0);
+
+    /* This vertex buffer will be called by a drawcall in the loping of the window */
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -36,12 +61,8 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f( 0.0f,  0.5f);
-        glVertex2f( 0.5f, -0.5f);
-        glEnd();
+        /* Drawcall the last bind vertex buffer defined*/
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
