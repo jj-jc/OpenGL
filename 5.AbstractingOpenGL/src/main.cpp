55b1baf1d6e34a2519f25c67598680d2b87c76f5
jj-jc <juanjosejuradocamino@gmail.com>
2022-05-31 17:06:42 +0200

    Rendering two textures simply drawing with different position uniform

--- 5.AbstractingOpenGL/src/main.cpp
+++ 5.AbstractingOpenGL/src/main.cpp
@@ -60,6 +60,9 @@
 // Convertion of images into buffer information
 #include "stb_image.h"
 
+// Tests
+#include "tests/TestClearColor.h"
+
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
@@ -133,21 +136,11 @@ std::cout << "------------------ Debug Mode ------------------" << std::endl;
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO();
-    (void)io;
-    // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable
-    // Keyboard Controls io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad; //
     // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-    // ImGui::StyleColorsClassic();
+    ImGui::StyleColorsDark(); // or  ImGui::StyleColorsClassic();
     // Setup Platform/Renderer backends
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
-    // Our state
-    bool show_demo_window = true;
-    // bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.0471, 0.5137, 0.6549, 1.0);
-    int display_w, display_h; // variables for glfwGetFramebufferSize() and control de size of the windows;
-
 
     // setting GL_DEBUG_OUTPUT
 #ifdef MY_DEBUG
@@ -162,7 +155,14 @@ std::cout << "------------------ Debug Mode ------------------" << std::endl;
     }
 #endif
 
+    // --------------------------------------
     // OpenGl tools
+    //
+    // For using blend feature
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glBlendEquation(GL_FUNC_ADD);
+
     glfwSetKeyCallback(window, key_callback);
 
     float positions[] = {
@@ -172,15 +172,10 @@ std::cout << "------------------ Debug Mode ------------------" << std::endl;
          100.0f, 300.0f, 0.0f,  0.0f, 1.0f, 1.0f,  0.0f, 1.0f,     // top left
          300.0f, 300.0f, 0.0f,  1.0f, 1.0f, 0.0f,  1.0f, 1.0f      // top right
     };
-
     unsigned int indices[] = {
         0, 1, 2,
         2, 3, 0
     };
-
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    glBlendEquation(GL_FUNC_ADD);
 
     // Vertex buffer object
     VertexBuffer vbo(positions, sizeof(positions));
@@ -195,11 +190,13 @@ std::cout << "------------------ Debug Mode ------------------" << std::endl;
     IndexBuffer ibo(indices, sizeof(indices));
 
     // MVP matrix
+    glm::vec3 translationA(0.0f, 0.0f, 0.0f);
+    glm::vec3 translationB(0.0f, 0.0f, 0.0f);
+    glm::vec3 scale(1.0f, 1.0f, 1.0f);
     glm::mat4 proj = glm::ortho(0.0f, 800.0f, 0.0f, 600.0f, -1.0f, 1.0f); // projects the view into a field of (16 x 16 normalized)
     glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0,0)); // translate the object to the left or translate the camera to the right
     glm::mat4 model = glm::mat4(1.0f);
     glm::mat4 mvp = proj * view * model;
-
 
     // Create a complete shader program (with vertex and fragment shaders)    
     Shader myShader(Shader::getShaderSource("/home/jjjurado/Dev/OpenGL/5.AbstractingOpenGL/res/shaders/triangle.vs"), 
@@ -210,17 +207,17 @@ std::cout << "------------------ Debug Mode ------------------" << std::endl;
     Texture texture("/home/jjjurado/Dev/OpenGL/5.AbstractingOpenGL/res/textures/Linux.jpeg"); 
     texture.bind(0); // bind to the slot we want to use.
     myShader.setUniform1i("u_Texture", 0); // this set to the slot of the texture we want to use in the uniform
-
     vao.unbind();
     vbo.unbind();
     ibo.unbind();
     myShader.unbind();  
-
     Renderer myRenderer;
-    glm::vec3 translation(0.0f, 0.0f, 0.0f);
-    glm::vec3 scale(1.0f, 1.0f, 1.0f);
 
-    glm::mat4 translationMat(1.0f);
+    // Our state
+    ImVec4 clear_color = ImVec4(0.0471, 0.5137, 0.6549, 1.0);
+    int display_w, display_h; // variables for glfwGetFramebufferSize() and control de size of the windows;
+
+    test::TestClearColor test;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -228,16 +225,25 @@ std::cout << "------------------ Debug Mode ------------------" << std::endl;
         /* Render here */
         myRenderer.clear();
         myShader.bind();
-        vao.bind();
-        ibo.bind();
         // Use the polygon mode. This affects how the objects are rasterized (4th step in the magic plumb)
         // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // works to debug if everything is drawing as it is suppose to
         // myShader.setUniform4f("u_Color", glm::vec4(0.8706, 0.7961, 0.1098, 0.5));
-        model = glm::translate(glm::mat4(1.0f), translation);
-        // model = glm::translate(glm::mat4(1.0f), translation) * glm::scale(translation, scale);
-        mvp = proj * view * model;
-        myShader.setUniformMatrix4fv("u_MVP", mvp);
-        myRenderer.draw(vao, ibo, myShader);
+        
+        {
+            model = glm::translate(glm::mat4(1.0f), translationA);
+            // model = glm::translate(glm::mat4(1.0f), translation) * glm::scale(translation, scale);
+            mvp = proj * view * model;
+            myShader.setUniformMatrix4fv("u_MVP", mvp);
+            myRenderer.draw(vao, ibo, myShader); // Although the vao stores the ibo too, it is more flexible if it is possible to change that information
+        }
+
+        {
+            model = glm::translate(glm::mat4(1.0f), translationB);
+            // model = glm::translate(glm::mat4(1.0f), translation) * glm::scale(translation, scale);
+            mvp = proj * view * model;
+            myShader.setUniformMatrix4fv("u_MVP", mvp);
+            myRenderer.draw(vao, ibo, myShader); // Although the vao stores the ibo too, it is more flexible if it is possible to change that information
+        }
 
         // select and draw the element buffer
         // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // draw with the element information, if it has no information. Segment fault (core dumped)
@@ -247,34 +253,34 @@ std::cout << "------------------ Debug Mode ------------------" << std::endl;
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
-
         // 1. Show the big demo window
         ImGui::ShowDemoWindow((bool*)true);
-        
         // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
         {
             ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
             ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
             // ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
             // ImGui::Checkbox("Another Window", &show_another_window);
-            ImGui::SliderFloat2("Translation", &translation.x, 0, SCR_WIDTH);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::SliderFloat2("translationA", &translationA.x, 0, SCR_WIDTH);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::SliderFloat2("translationB", &translationB.x, 0, SCR_WIDTH);            // Edit 1 float using a slider from 0.0f to 1.0f
             ImGui::SliderFloat2("Scale", &scale.x, 0, 1);            // Edit 1 float using a slider from 0.0f to 1.0f //FIXME:magic number
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             ImGui::End();
         }
-
         // Rendering
         ImGui::Render();
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w,
-                    clear_color.z * clear_color.w, clear_color.w);
-        // glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-        // Process of OpenGl
-        glfwSwapBuffers(window); // Swap front and back buffers
-        glfwPollEvents(); // Poll for and process events
+        // // OpenGL process
+        // glfwGetFramebufferSize(window, &display_w, &display_h);
+        // glViewport(0, 0, display_w, display_h);
+        // glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w,
+        //             clear_color.z * clear_color.w, clear_color.w);
+        // glClear(GL_COLOR_BUFFER_BIT);
+        // Swap front and back buffers
+        glfwSwapBuffers(window); 
+        // Poll for and process events
+        glfwPollEvents(); 
     }
 
     // Cleanup
