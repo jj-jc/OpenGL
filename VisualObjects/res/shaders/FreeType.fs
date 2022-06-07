6d04ae826e00a88871dc0b36a7f660be39a5a18f
jj-jc <juanjosejuradocamino@gmail.com>
2022-06-07 14:49:02 +0200

    Some comments about the frame shader

--- VisualObjects/res/shaders/FreeType.fs
+++ VisualObjects/res/shaders/FreeType.fs
@@ -8,6 +8,8 @@ uniform vec3 u_TextColor;
  
 void main()
 {
-    vec4 sampled = vec4(1.0, 1.0, 1.0, texture(u_Text, v_TexCoords).r);
+    // take red component for the bitmap texture (just one byte). Think as a color with the last component as the transparent.
+    vec4 sampled = vec4(1.0, 1.0, 1.0, texture(u_Text, v_TexCoords).r); 
+    // the u_TextColo has the maximum transparent and the sampled depends on the bitmap.
     f_Color = vec4(u_TextColor, 1.0) * sampled;
 } 
