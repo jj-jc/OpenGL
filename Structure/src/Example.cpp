97e22baaa5bdca9ff0a1e1c2de880bbb3f16276e
jj-jc <juanjosejuradocamino@gmail.com>
2022-06-22 10:19:37 +0200

    Formatter for Example and ExampleLayer

--- Structure/src/Example.cpp
+++ Structure/src/Example.cpp
@@ -1,55 +1,57 @@
 
 #include "CSU_MANAGER/VisualManager.h"
 #include "ExampleLayer.h"
-#include "MetPanelLayer.h"
 #include "Menu.h"
+#include "MetPanelLayer.h"
 
 // stb libraries of single-file header-file
 // image loader
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
 
-// log4cxx 
+// log4cxx
+#include "log4cxx/basicconfigurator.h"
+#include "log4cxx/helpers/pool.h"
 #include "log4cxx/log4cxx.h"
 #include "log4cxx/logger.h"
-#include "log4cxx/basicconfigurator.h"
 #include "log4cxx/propertyconfigurator.h"
-#include "log4cxx/helpers/pool.h"
 
 // Define static logger variable
-log4cxx::LoggerPtr loggerMain; 
+log4cxx::LoggerPtr loggerMain;
 log4cxx::LoggerPtr loggerOpenGL;
 // log4cxx::LoggerPtr loggerMetPanel;
 log4cxx::LoggerPtr loggerMenu;
 
-
-
-class Example : public VisualManager
+class Example: public VisualManager
 {
-public:
-	Example()
-		: VisualManager("OpenGL Examples")
-	{
-		pushOverLayer(new Menu("Menu One"));
-		pushLayer(new MetPanelLayer("MetPanelLayer One"));
-	}
+ public:
+   Example(): VisualManager("OpenGL Examples")
+   {
+      pushOverLayer(new Menu("Menu One"));
+      pushLayer(new MetPanelLayer("MetPanelLayer One"));
+   }
 };
 
 int main()
 {
-	loggerMain = log4cxx::LoggerPtr(log4cxx::Logger::getLogger("Main"));
-	loggerOpenGL = log4cxx::LoggerPtr(log4cxx::Logger::getLogger("OpenGL"));
-	// loggerMetPanel = log4cxx::LoggerPtr(log4cxx::Logger::getLogger("MetPanel"));
-	loggerMenu = log4cxx::LoggerPtr(log4cxx::Logger::getLogger("Menu"));
-	log4cxx::File pc("/home/jjjurado/Dev/OpenGL/Structure/conf/log4.cxx.properties");
-	log4cxx::BasicConfigurator::resetConfiguration(); 
-	log4cxx::PropertyConfigurator::configure (pc);
+   loggerMain   = log4cxx::LoggerPtr(log4cxx::Logger::getLogger("Main"));
+   loggerOpenGL = log4cxx::LoggerPtr(log4cxx::Logger::getLogger("OpenGL"));
+   // loggerMetPanel =
+   // log4cxx::LoggerPtr(log4cxx::Logger::getLogger("MetPanel"));
+   loggerMenu = log4cxx::LoggerPtr(log4cxx::Logger::getLogger("Menu"));
+   log4cxx::File pc(
+      "/home/jjjurado/Dev/OpenGL/Structure/conf/log4.cxx.properties");
+   log4cxx::BasicConfigurator::resetConfiguration();
+   log4cxx::PropertyConfigurator::configure(pc);
 
-	std::unique_ptr<Example> app = std::make_unique<Example>();
-	if(app){
-		LOG4CXX_INFO(loggerMain, "The app has been created!");
-	}else{
-		LOG4CXX_ERROR(loggerMain, "The app has not been created!");
-	}
-	app->run();
+   std::unique_ptr<Example> app = std::make_unique<Example>();
+   if(app)
+   {
+      LOG4CXX_INFO(loggerMain, "The app has been created!");
+   }
+   else
+   {
+      LOG4CXX_ERROR(loggerMain, "The app has not been created!");
+   }
+   app->run();
 }
--- Structure/src/ExampleLayer.cpp
+++ Structure/src/ExampleLayer.cpp
@@ -1,80 +1,81 @@
 #include "ExampleLayer.h"
 
-ExampleLayer::ExampleLayer()
-	:Layer("ExampleLayer")
-{
+ExampleLayer::ExampleLayer(): Layer("ExampleLayer") {}
+ExampleLayer::ExampleLayer(const std::string name): Layer(name) {}
 
-}
-ExampleLayer::ExampleLayer(const std::string name)
-	:Layer(name)
-{
-
-}
-
-ExampleLayer::~ExampleLayer()
-{
-
-}
+ExampleLayer::~ExampleLayer() {}
 
 void ExampleLayer::attach()
 {
-    //TODO:enable debugging
-    // EnableGLDebugging();
+   // TODO:enable debugging
+   // EnableGLDebugging();
 
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+   glEnable(GL_DEPTH_TEST);
+   glEnable(GL_BLEND);
+   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    // Create a complete shader program (with vertex and fragment shaders)    
-    static Shader myShader(Shader::getShaderSource("/home/jjjurado/Dev/OpenGL/Structure/res/shaders/triangle.vs"), 
-                    Shader::getShaderSource("/home/jjjurado/Dev/OpenGL/Structure/res/shaders/triangle.fs"));
-    
-	m_Shader = &myShader;
-	// myShader.use();
+   // Create a complete shader program (with vertex and fragment shaders)
+   static Shader myShader(
+      Shader::getShaderSource(
+         "/home/jjjurado/Dev/OpenGL/Structure/res/shaders/triangle.vs"),
+      Shader::getShaderSource(
+         "/home/jjjurado/Dev/OpenGL/Structure/res/shaders/triangle.fs"));
 
-	glCreateVertexArrays(1, &m_QuadVA);
-	glBindVertexArray(m_QuadVA);
+   m_Shader = &myShader;
+   // myShader.use();
 
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
-		 0.5f,  0.5f, 0.0f,
-		-0.5f,  0.5f, 0.0f
-	};
+   glCreateVertexArrays(1, &m_QuadVA);
+   glBindVertexArray(m_QuadVA);
 
-	glCreateBuffers(1, &m_QuadVB);
-	glBindBuffer(GL_ARRAY_BUFFER, m_QuadVB);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+   float vertices[] = {-0.5f,
+                       -0.5f,
+                       0.0f,
+                       0.5f,
+                       -0.5f,
+                       0.0f,
+                       0.5f,
+                       0.5f,
+                       0.0f,
+                       -0.5f,
+                       0.5f,
+                       0.0f};
 
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+   glCreateBuffers(1, &m_QuadVB);
+   glBindBuffer(GL_ARRAY_BUFFER, m_QuadVB);
+   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	uint32_t indices[] = { 0, 1, 2, 2, 3, 0 };
-	glCreateBuffers(1, &m_QuadIB);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_QuadIB);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+   glEnableVertexAttribArray(0);
+   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
+
+   uint32_t indices[] = {0, 1, 2, 2, 3, 0};
+   glCreateBuffers(1, &m_QuadIB);
+   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_QuadIB);
+   glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+                sizeof(indices),
+                indices,
+                GL_STATIC_DRAW);
 }
 
 void ExampleLayer::detach()
 {
-    glDeleteVertexArrays(1, &m_QuadVA);
-	glDeleteBuffers(1, &m_QuadVB);
-	glDeleteBuffers(1, &m_QuadIB);
+   glDeleteVertexArrays(1, &m_QuadVA);
+   glDeleteBuffers(1, &m_QuadVB);
+   glDeleteBuffers(1, &m_QuadIB);
 }
 
 void ExampleLayer::update(double ts)
 {
-	// glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-	// glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+   // glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+   // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
-
 
 void ExampleLayer::renderImGui()
 {
-	uintptr_t aux = reinterpret_cast<std::uintptr_t>(this);
-    ImGui::Begin((getName()+ "##" + std::to_string(aux)).c_str());
-	if (ImGui::ColorEdit4("Square Base Color", glm::value_ptr(m_SquareBaseColor)))
-		m_SquareColor = m_SquareBaseColor;
-	ImGui::ColorEdit4("Square Alternate Color", glm::value_ptr(m_SquareAlternateColor));
-	ImGui::End();
+   uintptr_t aux = reinterpret_cast<std::uintptr_t>(this);
+   ImGui::Begin((getName() + "##" + std::to_string(aux)).c_str());
+   if(ImGui::ColorEdit4("Square Base Color", glm::value_ptr(m_SquareBaseColor)))
+      m_SquareColor = m_SquareBaseColor;
+   ImGui::ColorEdit4("Square Alternate Color",
+                     glm::value_ptr(m_SquareAlternateColor));
+   ImGui::End();
 }
